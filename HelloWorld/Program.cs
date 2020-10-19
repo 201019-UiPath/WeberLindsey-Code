a832597298b11c434f0450a0a58cde246ff880ee
lw3b3r <daevahush@gmail.com>
2020-10-19 18:08:28 -0400

    Created Hello World Console App

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿//Predefined namespace
+using System;
+
+//To create custom defined namespace using namespace keyword
+namespace helloworld
+{
+    class Program
+    {
+        //Execution starts here
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! " + DateTime.Now);
+
+            string firstname, lastname;
+            Console.WriteLine("Please enter your first name: ");
+            firstname = Console.ReadLine(); //Takes user's input in string form
+            Console.WriteLine("Please enter your last name: ");
+            lastname = Console.ReadLine();
+            // Console.WriteLine("Welcome, " + name);
+            Console.WriteLine($"Welcome, {firstname} {lastname}"); //string interpolation
+        }
+    }
+}
