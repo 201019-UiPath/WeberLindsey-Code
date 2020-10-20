c7794bd3f1b300c16f328c4907138fe15e68e1e8
lw3b3r <daevahush@gmail.com>
2020-10-20 12:37:00 -0400

    Created project organization example

---
+++ HeroesApp/HeroesDB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HeroesDB
+{
+    public class Class1
+    {
+    }
+}
---
+++ HeroesApp/HeroesUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeroesUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
