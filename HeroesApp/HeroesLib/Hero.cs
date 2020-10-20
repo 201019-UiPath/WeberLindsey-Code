732246467b9e50d9349a6ca1a2ae858f1934b125
lw3b3r <daevahush@gmail.com>
2020-10-20 16:30:06 -0400

    Added array examples

--- HeroesApp/HeroesLib/Hero.cs
+++ HeroesApp/HeroesLib/Hero.cs
@@ -2,17 +2,46 @@
 
 namespace HeroesLib
 {
-    public class Hero
-    {
 
-        public int id; //value type -> System.Int32
+    #region old way of creating class members
+    /*public class Hero
+    {
+        int id; //value type -> System.Int32
+
+        public int Id{
+            get{ return id;}
+            set{ id = value; }
+        }
+
+        // public int Id{get; set;} // Autonomous property
 
         public string name;
 
-        //Constructor
+        //Default Constructor
         public Hero() {
             id = 1;
             name = "Batman";
         }
+
+        public Hero(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            
+        }
+    }*/
+    #endregion
+
+    #region modern way of creating class members
+    public class Hero{
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string[] superPowers = new string[10]; //1-D array
+
+        public int[][] ja = new int[3][]; //Jagged array
+        
     }
+    #endregion
 }
--- HeroesApp/HeroesUI/Program.cs
+++ HeroesApp/HeroesUI/Program.cs
@@ -7,9 +7,48 @@ namespace HeroesUI
     {
         static void Main(string[] args)
         {
+            #region default constructor
+            /*Hero obj = new Hero();
+            Console.WriteLine($"{obj.id} {obj.name}");*/
+            #endregion
 
+            #region paramaterized constructor
+            // Hero obj1 = new Hero(2, "Deadpool");
+            // Console.WriteLine($"{obj1.id} {obj1.name}"); //Cannot access the variables if they're not public
+            #endregion
+
+            #region Access via Properties
+            // Console.WriteLine(obj1.Id); //read property value
+            // obj1.Id = 3;
+            // Console.WriteLine($"New ID = {obj1.Id}"); //Write into a property
+            #endregion
+
+            #region accessing 1-D arrays
             Hero obj = new Hero();
-            Console.WriteLine($"{obj.id} {obj.name}");
+
+            /*Console.Write("Please enter your hero ID: ");
+            obj.Id = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your superhero name: ");
+            obj.name = Console.ReadLine();
+
+            Console.Write("Enter the first power: ");
+            obj.superPowers[0] = Console.ReadLine();
+
+            Console.Write($"{obj.Id} {obj.name} {obj.superPowers[0]}");*/
+            #endregion
+
+            #region Accessing Jagged arrays
+            obj.ja[0] = new int[2]; //first column
+            obj.ja[1] = new int[3]; //second column
+            obj.ja[2] = new int[1]; //third column
+
+            obj.ja[0][0] = 10; //initialize value of first row and first column to 10
+            obj.ja[1][2] = 15;
+            
+            Console.Write(obj.ja.Rank); //rank is the dimension of the array
+            Console.Write(obj.ja.Length); //length is elements of the arrays
+            #endregion
 
         }
     }
