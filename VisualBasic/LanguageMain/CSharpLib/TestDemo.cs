e55200c9b7470365814f0743df42ccde8eb78b25
lw3b3r <daevahush@gmail.com>
2020-11-17 17:26:41 -0500

    VB Demo

---
+++ VisualBasic/LanguageMain/CSharpLib/TestDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSharpLib
+{
+    public class TestDemo
+    {
+        public void GetFullName(string firstName, string middleName, string lastName) { 
+            if(middleName != "" || middleName != null)
+            {
+                Console.WriteLine($"{firstName} {middleName} {lastName}");
+            } else
+            {
+                Console.WriteLine($"{firstName} {lastName}");
+            }
+        }
+
+        public List<string> LoadFile()
+        {
+            List<string> output = new List<string>();
+            List<string> lines = File.ReadAllLines(@"C:\Users\Lin\Documents\Revature\WeberLindsey-Code\VisualBasic\LanguageMain\Text.txt").ToList();
+
+            for(int i = 0; i < lines.Count-1; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    output.Add(lines[i]);
+                }
+            }
+
+            return output;
+        }
+
+
+    }
+}
---
+++ VisualBasic/LanguageMain/LanguageMain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LanguageMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpLib.TestDemo testcsharp = new CSharpLib.TestDemo();
+            testcsharp.GetFullName("Lindsey", "Nicole", "Weber");
+            var resCsharp = testcsharp.LoadFile();
+            Console.WriteLine(resCsharp);
+            
+
+            Console.WriteLine("~~~~~~~~~~~~");
+
+            VbLib.TestDemo testvb = new VbLib.TestDemo();
+            testvb.GetFullName("Joseph", "Arnold", "Petrie");
+            var resVb = testvb.LoadFile();
+            Console.WriteLine(resVb);
+            
+
+
+        }
+    }
+}
